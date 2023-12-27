e754e70fea3a5cb6a22c7207827767bf10099066
Cinthya Urbina <100502590+Celizabet@users.noreply.github.com>
2023-12-27 14:31:30 -0600

    Add files via upload
    Extended URLs file

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,17 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 from .views import (
     ProductAPIView,
-    ProductDetailAPIView
+    ProductDetailAPIView,
+    ProductApiViewSet
 )
+
+router = DefaultRouter() 
+router.register("api-setview", ProductApiViewSet, basename="api-setview")
 
 urlpatterns = [
     path("", ProductAPIView.as_view(), name="api_list"),
-    path("<int:pk>", ProductDetailAPIView.as_view(), name="api_detail")
+    path("<int:pk>", ProductDetailAPIView.as_view(), name="api_detail"),
+    path("", include(router.urls))
 ]
